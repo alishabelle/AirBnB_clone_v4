c80ec4b2a51bc6368122f61a0527451401641217
tyedge <tyedge.inc@gmail.com>
2019-10-05 03:37:06 +0000

    Adding Task3 files

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,18 @@
+$(function () {
+  const amen = {};
+  if ($('input[type=checkbox]').prop(':checked')) {
+    amen[$(this).data('id')] = $(this).data('name');
+  } else {
+    delete amen[$(this).data('id')];
+  }
+  const vals = Object.values(amen);
+  if (vals.length > 0) {
+    $('.amenities > h4').text(vals.join(', '));
+  }
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(url, function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    }
+  });
+});
