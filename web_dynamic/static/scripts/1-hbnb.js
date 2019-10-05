3da6fb79018ed8eeb53ee323185738f3a5ac4d3b
tyedge <tyedge.inc@gmail.com>
2019-10-05 03:42:48 +0000

    Updating 1-hbnb.js with semistandard compliant version

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,12 @@
 $(function () {
-    let amen = {};
-    if ($("input[type=checkbox]").prop(":checked")) {
-	amen[$(this).data('id')] = $(this).data('name');
-    } else {
-	delete amen[$(this).data('id')];
-    }
-    let vals = Object.values(amen);
-    if (vals.length > 0) {
-      $('.amenities > h4').text(vals.join(', '));
-    }
- });
+  const amen = {};
+  if ($('input[type=checkbox]').prop(':checked')) {
+    amen[$(this).data('id')] = $(this).data('name');
+  } else {
+    delete amen[$(this).data('id')];
+  }
+  const vals = Object.values(amen);
+  if (vals.length > 0) {
+    $('.amenities > h4').text(vals.join(', '));
+  }
+});
